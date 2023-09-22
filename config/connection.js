ba02fcd260479c4898bec9b6fa4d688a5533281e
Meriam <132971614+msadio8@users.noreply.github.com>
2023-09-22 18:55:23 +1000

    revise connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,18 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/FriendsDB';
 
-connect(connectionString);
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(connectionString);
+    console.log('Connected to the database.');
+  } catch (error) {
+    console.error('Database connection error:', error);
+    process.exit(1); // Exit the process on connection error
+  }
+}
 
-module.exports = connection;
+connectToDatabase(); // Connect to the database on application startup
+
+module.exports = mongoose.connection;
