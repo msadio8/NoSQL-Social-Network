7ba29f66876a45aa5be65751e766f9d947189b03
Meriam <132971614+msadio8@users.noreply.github.com>
2023-09-22 18:52:26 +1000

    add new code to user.js

---
+++ models/user.js
@@ -0,0 +1,52 @@
+const { Schema, model, Types } = require("mongoose");
+
+// Define the User schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(v);
+        },
+      },
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true, 
+    },
+    id: false, 
+  }
+);
+
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+
+const user = model("user", userSchema);
+
+
+module.exports = user;
